6b1ddb8379f2deabffca46ba573c023ba82b20a0
rahulraj38019-code <rahulraj38019@gmail.com>
2026-04-14 11:00:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
-const cacheName = 'rozgar-v1';
-const assets = ['./', './index.html'];
+const CACHE_NAME = 'rozgar-v2';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+const STATIC_ASSETS = [
+  '/',
+  '/manifest.json',
+  '/sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
