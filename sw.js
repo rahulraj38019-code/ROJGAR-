1cf151c3e33ab11977f32068d6a5f191cdbadb58
rahulraj38019-code <rahulraj38019@gmail.com>
2026-04-20 16:12:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,18 @@
-const CACHE_NAME = 'vidyajobs-ai-v2';
+const cacheName = 'vidyajobs-basic-v1';
 
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/sw.js'
-];
-
-// Install Service Worker
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
 });
 
-// Activate & clean old cache
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
-// Fetch handler (Network first, fallback to cache)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        // Clone & store fresh response
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
