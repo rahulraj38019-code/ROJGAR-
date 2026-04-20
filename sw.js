358891ffa38b68c3f5e5d161de0bd49b70c309aa
rahulraj38019-code <rahulraj38019@gmail.com>
2026-04-20 16:16:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-const cacheName = 'vidyajobs-basic-v1';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (event) => event.respondWith(fetch(event.request)));
